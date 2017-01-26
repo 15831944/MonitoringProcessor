bb15a8a16226c40b66e04ad6bf31f5097b8b0e28
PHL <sgw32@yandex.ru>
2017-01-26 11:28:45 +0400

    First commit

---
+++ MProc/MProc.cpp
@@ -0,0 +1,18 @@
+// MProc.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	if(argc <= 1)
+    {
+        std::cout << "usage: " << argv[0] << " filename" << std::endl;
+        return 0;
+    }
+
+	return 0;
+}
+
